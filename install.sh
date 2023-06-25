5d02b6d10dc705bbf032ac44161357a2fc56930f
Mihail <mik.gryzunov@gmail.com>
2023-06-25 19:33:41 +0300

    fix command

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec nextcloud-app bash -c "apt update && apt install smbclient -y && rm -rf /var/lib/apt/lists/*"
+docker exec nextcloud-app bash -c "apt update && apt install smbclient -y && rm -rf /var/lib/apt/lists/*"
